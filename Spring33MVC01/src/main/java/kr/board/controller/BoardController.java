d0a55c46198dbbf26a2ca9fe608c608e981767dc
bitcocom <bitcocom@empas.com>
2023-03-10 12:30:24 +0900

    gsm33

--- Spring33MVC01/src/main/java/kr/board/controller/BoardController.java
+++ Spring33MVC01/src/main/java/kr/board/controller/BoardController.java
@@ -1,5 +1,20 @@
 package kr.board.controller;
 
-public class BoardController {
+import org.springframework.web.bind.annotation.RequestMapping;
 
+public class BoardController {
+   
+	// /list.do 요청이 오면 처리하는 메서드 정의
+	@RequestMapping("/list.do")
+	public String list() {
+		
+		return "";
+	}
+	
+	@RequestMapping("/register.do")
+    public String register() {
+		
+		return "";
+	}
+	
 }
--- Spring33MVC01/src/main/java/kr/board/entity/Board.java
+++ Spring33MVC01/src/main/java/kr/board/entity/Board.java
@@ -1,14 +1,55 @@
 package kr.board.entity;
 
 import java.util.Date;
-
 // 게시판(Object) : 번호,제목,내용,작성자,작성일,조회수.......
-public class Board {
+public class Board extends Object{
   private int num;
   private String title;
   private String content;
   private String writer;
   private Date indate;
   private int count;
-  
+  // setter, getter -> Lombok API
+public int getNum() {
+	return num;
+}
+public void setNum(int num) {
+	this.num = num;
+}
+public String getTitle() {
+	return title;
+}
+public void setTitle(String title) {
+	this.title = title;
+}
+public String getContent() {
+	return content;
+}
+public void setContent(String content) {
+	this.content = content;
+}
+public String getWriter() {
+	return writer;
+}
+public void setWriter(String writer) {
+	this.writer = writer;
+}
+public Date getIndate() {
+	return indate;
+}
+public void setIndate(Date indate) {
+	this.indate = indate;
+}
+public int getCount() {
+	return count;
+}
+public void setCount(int count) {
+	this.count = count;
+}
+  // toString()
+@Override
+public String toString() {
+	return "Board [num=" + num + ", title=" + title + ", content=" + content + ", writer=" + writer + ", indate="
+			+ indate + ", count=" + count + "]";
+}
 }
