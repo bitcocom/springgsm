e04e834f2e803c6dbe15682cb7e118d79ad985ae
bitcocom <bitcocom@empas.com>
2023-03-21 11:09:00 +0900

    gsm34

--- Spring34MVC01/src/main/java/kr/board/controller/BoardController.java
+++ Spring34MVC01/src/main/java/kr/board/controller/BoardController.java
@@ -5,14 +5,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 // Controller
 @Controller
-public class BoardController {
+public class BoardController { // new BoardController();
 
 	// 요청-->처리메서드 : 핸들러매핑(HandlerMapping)
 	// [게시판리스트보기 요청]이 오면 처리하는 [메서드]를 만들자
 	@RequestMapping("/list.do")
 	public String list() {
 		// 데이터베이스에서 게시물을 가져오는 부분....
-		// list.jsp 넘겨주는것
+		// list.jsp 넘겨주는것(ViewResolver)
 		return "board/list"; // /WEB-INF/views/board/list.jsp
 	}
 }
