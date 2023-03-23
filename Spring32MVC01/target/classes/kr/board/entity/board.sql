6131515fff529b9424b743bce4f6089d5c0377f1
bitcocom <bitcocom@empas.com>
2023-03-23 12:31:15 +0900

    gsm32

---
+++ Spring32MVC01/target/classes/kr/board/entity/board.sql
@@ -0,0 +1,19 @@
+create table board(
+  num int not null auto_increment,
+  title varchar(100) not null,
+  content varchar(2000) not null,
+  writer varchar(50) not null,
+  indate datetime default now(),
+  count int default 0,
+  primary key(num)
+);
+insert into board(title, content, writer)
+values('스프링 게시판 연습','스프링 게시판 연습','관리자');
+insert into board(title, content, writer)
+values('스프링 게시판 연습','스프링 게시판 연습','박매일');
+insert into board(title, content, writer)
+values('스프링 게시판 연습','스프링 게시판 연습','홍길동');
+
+select * from board;
+
+
