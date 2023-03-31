f7ff027f2edbd96fc8b0d2eb1adfd73bc1d31065
bitcocom <bitcocom@empas.com>
2023-03-31 12:09:59 +0900

    gsm33

--- Spring33MVC01/src/main/java/kr/board/controller/BoardController.java
+++ Spring33MVC01/src/main/java/kr/board/controller/BoardController.java
@@ -4,30 +4,25 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.board.entity.Board;
+import kr.board.mapper.BoardMapper;
 
 @Controller
 public class BoardController {
    
+	@Autowired
+	private BoardMapper mapper;
 	// /list.do 요청이 오면 처리하는 메서드 정의
 	// HandlerMapping
 	@RequestMapping("/list.do")
 	public String list(Model model) {
 		// Controller에서 View로 데이터를 넘기는 방법
-		Board vo=new Board();
-		vo.setNum(1);
-		vo.setTitle("게시판 출력하기");
-		vo.setWriter("이름");
-		vo.setIndate(new Date());
-		vo.setCount(0);
-		List<Board> list=new ArrayList<>();
-		list.add(vo);
-		list.add(vo);
-		list.add(vo);
+		List<Board> list=mapper.getList();
 		model.addAttribute("list", list);
 		return "board/list"; //->/WEB-INF/views/board/list.jsp:ViewResolver
 	}	
