c4ff26132c735b95c5b4bef5b93658b5dd7b8808
bitcocom <bitcocom@empas.com>
2023-04-04 12:29:35 +0900

    gsm34

--- Spring34MVC01/src/main/java/kr/board/mapper/BoardMapper.java
+++ Spring34MVC01/src/main/java/kr/board/mapper/BoardMapper.java
@@ -2,12 +2,13 @@ package kr.board.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import kr.board.entity.Board;
 // BoardMapper interface <----> SQL Mapper XML file
 public interface BoardMapper {
   
 	// 전체 게시물을 가져오는 메서드 정의
 	public List<Board> getLists(); // 추상메서드
-	
 	
 }
