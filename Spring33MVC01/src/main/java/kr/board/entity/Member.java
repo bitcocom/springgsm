185c15027a9f91fcfa02bba183302c32ed6ebb7a
bitcocom <bitcocom@empas.com>
2023-04-28 10:22:32 +0900

    gsm33

--- Spring33MVC01/src/main/java/kr/board/entity/Member.java
+++ Spring33MVC01/src/main/java/kr/board/entity/Member.java
@@ -1,11 +1,13 @@
 package kr.board.entity;
 
+import lombok.Data;
+
 @Data
 public class Member {
+	private int idx;
 	private String username; // id
 	private String password;
 	private String name;
-	private String email;
-	private String phone;
+	private String email;	
 	// setter, getter ~~~ : Lombok API
 }
