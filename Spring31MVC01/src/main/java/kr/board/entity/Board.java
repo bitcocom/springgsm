35520a8044f5e06fe8f21f01e95629e0fad956cb
bitcocom <bitcocom@empas.com>
2023-05-01 10:22:40 +0900

    gsm31

--- Spring31MVC01/src/main/java/kr/board/entity/Board.java
+++ Spring31MVC01/src/main/java/kr/board/entity/Board.java
@@ -2,7 +2,10 @@ package kr.board.entity;
 
 import java.util.Date;
 
+import lombok.Data;
+
 // 게시판(Object)->번호,제목,내용,작성자,작성일,조회수......
+@Data
 public class Board {
   private int num;
   private String title;
@@ -10,55 +13,5 @@ public class Board {
   private String writer;
   private Date indate;
   private int count;
-  public Board() {  }
-  public Board(int num, String title, String content, String writer, Date indate, int count) {
-	super();
-	this.num = num;
-	this.title = title;
-	this.content = content;
-	this.writer = writer;
-	this.indate = indate;
-	this.count = count;
-  }
-	public int getNum() {
-		return num;
-	}
-	public void setNum(int num) {
-		this.num = num;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public String getWriter() {
-		return writer;
-	}
-	public void setWriter(String writer) {
-		this.writer = writer;
-	}
-	public Date getIndate() {
-		return indate;
-	}
-	public void setIndate(Date indate) {
-		this.indate = indate;
-	}
-	public int getCount() {
-		return count;
-	}
-	public void setCount(int count) {
-		this.count = count;
-	}
-	@Override
-	public String toString() {
-		return "Board [num=" + num + ", title=" + title + ", content=" + content + ", writer=" + writer + ", indate="
-				+ indate + ", count=" + count + "]";
-	}
+  
 }
--- Spring31MVC01/src/main/java/kr/board/entity/board.sql
+++ Spring31MVC01/src/main/java/kr/board/entity/board.sql
@@ -16,3 +16,32 @@ insert into board(title, content, writer)
 values('스프링게시판','스프링게시판','홍길동');
 
 select * from board;
+
+create table member(
+  idx int auto_increment primary key,
+  username varchar(255) not null unique,
+  password varchar(255) not null,
+  name varchar(50) not null,
+  email varchar(50) not null unique
+);
+
+insert into member(username, password, name, email)
+values('gsm01','gsm01','관리자', 'admin@gsm.kr');
+
+insert into member(username, password, name, email)
+values('gsm02','gsm02','박매일', 'park@gsm.kr');
+
+insert into member(username, password, name, email)
+values('gsm03','gsm03','홍길동', 'hong@gsm.kr');
+
+select * from member;
+
+
+
+
+
+
+
+
+
+
