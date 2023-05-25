b27de9220e627fc3be907dcb66b1704fae750079
bitcocom <bitcocom@empas.com>
2023-05-25 12:21:46 +0900

    gsm32

--- Spring32MVC01/src/main/java/kr/board/entity/PageMaker.java
+++ Spring32MVC01/src/main/java/kr/board/entity/PageMaker.java
@@ -1,5 +1,4 @@
 package kr.board.entity;
-
 import lombok.Data;
 
 @Data
@@ -17,6 +16,28 @@ public class PageMaker {
 	   makePage();
    }
    public void makePage() {
-	   
+	 // 1.선택한 페이지의 화면에 보여질 마지막 페이지 번호
+	 endPage=(int)(Math.ceil(cri.getPage()/(double)displayPageNum)*displayPageNum);
+	 // 2.선택한 페이지의 화면에 보여질 시작 페이지 번호
+	 startPage=(endPage-displayPageNum)+1;  
+	 if(startPage<=0) startPage=1;
+	 // 3.전체 마지막 페이지 계산(31)
+	 int tempEndPage=(int)(Math.ceil(totalCount/(double)cri.getPerPageNum()));
+	 // 4.화면에 보여질 마지막 페이지 유효성 체크
+	 if(tempEndPage<endPage) {
+			endPage=tempEndPage;		
+	 }
+	 
    }
 }
+
+
+
+
+
+
+
+
+
+
+
