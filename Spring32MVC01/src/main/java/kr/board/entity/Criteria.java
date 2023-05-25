b535f3c7c464f934b3de86eefd191d744bae7a44
bitcocom <bitcocom@empas.com>
2023-05-25 10:18:31 +0900

    gsm32

---
+++ Spring32MVC01/src/main/java/kr/board/entity/Criteria.java
@@ -0,0 +1,19 @@
+package kr.board.entity;
+
+import lombok.Data;
+// 페이징 처리에 기준클래스
+@Data
+public class Criteria {
+  private int page;
+  private int perPageNum; 
+  // 초기화
+  public Criteria() {
+	  page=1;
+	  perPageNum=10;
+  }
+  // 선택한 페이지에 해당하는 게시글의 시작번호를 구하기
+  // 1page: 0~perPageNum, 2page: 10~perPageNum, 3page: 20~perPageNum 
+  public int getPageStart() {
+	 return (page-1)*perPageNum; 	  
+  }
+}
