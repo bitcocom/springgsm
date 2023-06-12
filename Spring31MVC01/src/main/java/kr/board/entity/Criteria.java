897aed3ae5ef47a550f437178355f4d2f5a483ed
bitcocom <bitcocom@empas.com>
2023-06-12 10:16:36 +0900

    gsm31

---
+++ Spring31MVC01/src/main/java/kr/board/entity/Criteria.java
@@ -0,0 +1,22 @@
+package kr.board.entity;
+
+import lombok.Data;
+
+@Data
+public class Criteria {
+  private int page;
+  private int perPageNum;
+  public Criteria() {
+	  this.page=1;
+	  this.perPageNum=10;// 3
+  }
+  // 선택한 페이지에 해당하는 게시글의 시작번호 구하기
+  // 1page: select * from reply order by bgroup desc, bseq asc limit 0, 10 
+  // 2page: select * from reply order by bgroup desc, bseq asc limit 10, 10
+  // 3page: select * from reply order by bgroup desc, bseq asc limit 20, 10
+  // select * from reply order by bgroup desc, bseq asc 
+  // limit ${startNum}, ${perPageNum}
+  public int getStartNum() {
+	  return (page-1)*perPageNum;
+  }  
+}
